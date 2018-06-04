b8cb8e96b50c2aa6a5b79a7700cb56c61b389a44
Md Rezwan Islam <34816575+muhammadrezwanislam@users.noreply.github.com>
2018-06-04 18:29:34 -0400

    Update sentiment analysis.R

--- sentiment analysis.R
+++ sentiment analysis.R
@@ -35,52 +35,6 @@ clean_tweet = gsub("http\\w+", "", clean_tweet)
 clean_tweet = gsub("[ \t]{2,}", "", clean_tweet)
 clean_tweet = gsub("^\\s+|\\s+$", "", clean_tweet)
 
-# define "tolower error handling" function 
-try.error = function(x)
-{
-  # create missing value
-  y = NA
-  # tryCatch error
-  try_error = tryCatch(tolower(x), error=function(e) e)
-  # if not an error
-  if (!inherits(try_error, "error"))
-    y = tolower(x)
-  # result
-  return(y)
-}
-
-# lower case using try.error with sapply 
-clean_tweet = sapply(clean_tweet, try.error)
-
-head(clean_tweet)
-
-# classify emotion
-class_emo = classify_emotion(clean_tweet, algorithm="bayes", prior=1.0)
-# get emotion best fit
-emotion = class_emo[,7]
-# substitute NA's by "unknown"
-emotion[is.na(emotion)] = "unknown"
-
-# classify polarity
-class_pol = classify_polarity(clean_tweet, algorithm="bayes")
-# get polarity best fit
-polarity = class_pol[,4]
-
-# data frame with results
-sent_df = data.frame(text=clean_tweet, emotion=emotion,
-                     polarity=polarity, stringsAsFactors=FALSE)
-
-# sort data frame
-sent_df = within(sent_df,
-                 emotion <- factor(emotion, levels=names(sort(table(emotion), decreasing=TRUE))))
-
-head(sent_df)
-# plot distribution of emotions
-library(ggplot2)
-ggplot(sent_df, aes(x=emotion)) +
-  geom_bar(aes(y=..count.., fill=emotion)) +
-  scale_fill_brewer(palette="Dark2") +
-  labs(x="emotion categories", y="number of tweets",title = "Sentiment Analysis of Tweets about Starbucks\n(classification by emotion)")
 
 
 
@@ -89,8 +43,6 @@ library(syuzhet)
 a<-get_nrc_sentiment(clean_tweet[1:50000])
 write.csv(a, "sentimentscore.csv")
 
-#draw barplot using default r system
-#barplot(100*colSums(a)/sum(a),, las = 2, col=rainbow(10), ylab = 'count', main = '% of sentiment')
 
 #draw barplot using ggplot2 library 
 Sentimentscore <- data.frame(colSums(a[,]))
